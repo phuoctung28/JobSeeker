475ab8cf0e43fc07bd544142eca40762f0a5e107
Phuoc Tung <nguyenphuoctung.284@gmail.com>
2022-05-31 20:06:59 +0700

    delete server

--- client/src/containers/Home/index.js
+++ client/src/containers/Home/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { Header } from '../../layouts/Header'
-
+import "./index.module.css"
 export const Homepage = () => {
   return (
+    <div className='vh-100'>
     <div>
-      <Header />
+    <Header id="homepage"/>
+    <div className='d-flex'>
+    <div>
+      <h1>Discover Your <br></br> <p className='text'>Dream</p> Job</h1>
+    </div>
+    <img src="/logo.svg" alt="" />
+    </div>
+    
+    </div>
+      
+
     </div>
   )
 }
