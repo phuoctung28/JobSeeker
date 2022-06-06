60da0051bbd72001782bea077d8c10b97b8d013e
Phuoc Tung <nguyenphuoctung.284@gmail.com>
2022-06-06 09:26:33 +0700

    create job page

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -12,15 +12,17 @@ export const Input = (props) => {
     rest,
     children,
     symbolClass,
-    className
+    className,
+    groupClasses
   } = props;
   const onChangeInputField = (e) => {
     onChangeHandler(e.target.value);
   };
   const classes = "py-2 " + className;
+  const inputGroupClasses = "mb-3 " + groupClasses;
   return (
     <>
-      <InputGroup className="mb-3">
+      <InputGroup className={inputGroupClasses}>
         <InputGroup.Text className={symbolClass} id="basic">{symbol}</InputGroup.Text>
         <FormControl
           className={classes}
--- client/src/containers/Job/index.jsx
+++ client/src/containers/Job/index.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import { Header } from '../../layouts/Header'
-
+import React from "react";
+import { CategoryList } from "../../components/Card/Category/CategoryList";
+import { Input } from "../../components/Input";
+import { Footer } from "../../layouts/Footer";
+import { Header } from "../../layouts/Header";
+import "./index.css";
 export const JobPage = () => {
   return (
     <div className="vh-100">
-    <div className="header py-5">
-      <div className="container">
-        <Header id="homepage" />
+      <div className="header py-5">
+        <div className="container">
+          <Header id="homepage" />
+          <div className="d-flex ml-5 flex-row justify-content-between mt-5 ">
+            <div className="mr-5">
+              <h1 className="d-inline">
+                Discover The <p className="text"> OJT Jobs</p>
+              </h1>
+            </div>
+          </div>
+        </div>
+        <Input
+          symbol={<i class="fa fa-search"></i>}
+          groupClasses="w-50 mx-auto my-0"
+          className="py-4 px-5 shadow-sm mx-auto my-0 w-50"
+          placeholder="Search for job title"
+        >
+          <button className="px-2 button">Search</button>
+        </Input>
       </div>
+      <div className="content my-5">
+        <div className="container">
+          <div>
+            <h4 className="my-4">Categories</h4>
+            <CategoryList />
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
-  </div>
-  )
-}
+  );
+};
