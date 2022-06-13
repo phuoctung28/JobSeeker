4bdc9f9ad68a8c2318110f8aeff4649b65904dcd
Phuoc Tung <nguyenphuoctung.284@gmail.com>
2022-06-13 17:43:00 +0700

    recode header

--- client/src/layouts/Header/index.jsx
+++ client/src/layouts/Header/index.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
     <header>
       <Navbar expand="lg" className="header">
         <Container>
-          <Navbar.Brand href="/">
+          <Navbar.Brand href="/home">
             {" "}
             <img
               src="/logo-fpt-login.png"
@@ -19,7 +19,7 @@ export const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto justify-content-center">
-              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/home">Home</Nav.Link>
               <Nav.Link href="/job">Jobs</Nav.Link>
               <Nav.Link href="/company">Company</Nav.Link>
             </Nav>
