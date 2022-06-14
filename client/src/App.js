c61cec7f58c228d9389000d1daabbc8b6535f820
Phuoc Tung <nguyenphuoctung.284@gmail.com>
2022-06-14 16:57:07 +0700

    add 404 not found

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,13 @@ import { CompanyPage } from "./containers/Company";
 import { JobDetail } from "./containers/JobDetail";
 import { useEffect } from "react";
 import { Profile } from "./containers/Profile";
+import { NotFound } from "./containers/NotFound";
 function App() {
   return (
     <div className="vh-100">
       <BrowserRouter>
         <Routes>
+          <Route path="*" element={<NotFound />} />
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Homepage />} />
           <Route path="/job" element={<JobPage />} />
