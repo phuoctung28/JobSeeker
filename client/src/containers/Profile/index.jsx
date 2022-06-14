1e5527c5269c5943d3cd93db5e09c9332d693752
dqt392 <tuandqse160189@fpt.edu.vn>
2022-06-14 10:45:45 +0700

    temporary banner

--- client/src/containers/Profile/index.jsx
+++ client/src/containers/Profile/index.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import classes from "./Profile.module.css";
 import { Header } from "../../layouts/Header";
+import { Banner } from "../../components/Banner";
 
 export const Profile = () => {
   return (
@@ -8,6 +9,7 @@ export const Profile = () => {
       <div className={classes.header}>
         <Header id="homepage" />
       </div>
+      <Banner></Banner>
     </Fragment>
   );
 };
