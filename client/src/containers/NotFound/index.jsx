33eb958e7257272729dcd73e3f7350df3b3c9b2f
dqt392 <tuandqse160189@fpt.edu.vn>
2022-06-14 19:59:46 +0700

    Not Found Page

--- client/src/containers/NotFound/index.jsx
+++ client/src/containers/NotFound/index.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { Fragment } from "react";
+
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <Fragment>
+      {/* <div className={classes.header}>
+        <Header></Header>
+      </div> */}
+
+      <img
+        src="/404NotFound.png"
+        width={900}
+        height={800}
+        alt=""
+        className="image img-fluid"
+      />
+
+      <img src="/svg.png" alt="" className="wave img-fluid" />
+    </Fragment>
+  );
+};
