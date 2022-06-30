f3b4daa36fcf9b3397d4cd613ee85ea6620546cd
dqt392 <tuandqse160189@fpt.edu.vn>
2022-06-30 15:44:52 +0700

    no

--- client/src/components/Card/Feature/FeatureList/index.jsx
+++ client/src/components/Card/Feature/FeatureList/index.jsx
@@ -2,27 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Feature } from "../";
 import { mockData } from "../../../../mockData";
 export const FeatureList = ({ jobList }) => {
-  let jobListss = mockData;
-  // console.log(jobList[0]);
-  // useEffect(() => {
-  //   if (jobList) {
-  //     effect
-  //   }
-  //   return () => {
-  //     cleanup
-  //   };
-  // }, [input]);
   return (
     <div className="feature__list">
-<<<<<<< HEAD
-      {jobListss.map((job) => (
-        <Feature job={job} />
-=======
       {jobList.map((job, index) => (
-        <Feature job={job} key={index}/>
->>>>>>> d92995fcbe675ed9ba5bcf788d95434e528f0ac1
+        <Feature job={job} key={index} />
       ))}
-      {/* <Feature job={jobList[0]} /> */}
     </div>
   );
 };
